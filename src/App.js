91ff869ad4c99c17f095bd059ca75722ec270bd2
hempy <hempy.scrymetto@gmail.com>
2019-09-12 18:09:36 +0300

    small things for text blocks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import "./App.css";
+import "./components/text/text.css";
+import "./components/text/text_error.css";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import {LoginPage} from "./pages/Login";
 import Profile from "./pages/Profile";
