7eed4cad493d33127a418b3d7672c36fce831580
hempy <hempy.scrymetto@gmail.com>
2019-09-16 17:59:25 +0300

    dots in error messages

--- src/components/form/__validationSchema/form__validationSchema_login.js
+++ src/components/form/__validationSchema/form__validationSchema_login.js
@@ -2,8 +2,8 @@ import * as Yup from 'yup';
 
 export const validationSchema_login = Yup.object().shape({
     email: Yup.string()
-        .email('Invalid email')
-        .required('Email is required'),
+        .email('Invalid email.')
+        .required('Email is required.'),
     password: Yup.string()
-        .required('Password is required'),
+        .required('Password is required.'),
 });
--- src/components/form/__validationSchema/form__validationSchema_newUser.js
+++ src/components/form/__validationSchema/form__validationSchema_newUser.js
@@ -3,11 +3,11 @@ import * as Yup from 'yup';
 export const validationSchema_newUser = Yup.object().shape({
     login: Yup.string(),
     email: Yup.string()
-        .email('Invalid email')
-        .required('Email is required'),
+        .email('Invalid email.')
+        .required('Email is required.'),
     password: Yup.string()
-        .required('Password is required'),
+        .required('Password is required.'),
     repeatPassword: Yup.string()
-        .oneOf([Yup.ref('password'), null], "Passwords must match")
+        .oneOf([Yup.ref('password'), null], "Passwords must match.")
         .required('Please, repeat the password.')
 });
