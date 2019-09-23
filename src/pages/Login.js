31b4a8fe3d2a81dd5ada319290d05cd6f50be1db
hempy <hempy.scrymetto@gmail.com>
2019-09-23 20:09:22 +0300

    <LoginPage/> without <LoginForm/> and <NewLoginForm/>

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,51 +3,14 @@ import React, {Fragment, useState} from "react";
 import "../components/text/text_link.css";
 import "../components/container/margin_15.css";
 
-import {Form} from "../components/form/form"
 import {Card} from "../components/card/card";
 import {Popup} from "../components/popup/popup";
-import {validationSchema_login} from '../components/form/__validationSchema/form__validationSchema_login'
-import {validationSchema_newUser} from '../components/form/__validationSchema/form__validationSchema_newUser'
-import {useAuth} from "../helpers/auth&route/authContext";
-import {submitForm} from "../helpers/submitForm";
-
-const Login = ({className}) => {
-    let template = ['email', 'password'];
-    let [isError, setError] = useState(false);
-    let {setAuthToken} = useAuth();
-    let onSubmit = (values) => submitForm(values, template, 'POST', '/', setAuthToken, setError);
-
-    return (<Fragment>
-            {isError && <div className='container'><p className='text text_error'>Something was wrong.</p></div>}
-            <Form className={className}
-                  input={[{email: 'email'}, {password: 'password'}]}
-                  initial={{email: '', password: ''}}
-                  validationSchema={validationSchema_login}
-                  onSubmit={onSubmit}
-            />
-        </Fragment>
-    )
-};
-
-const NewUserForm = ({className, endRegistration}) => {
-    let template = ['login', 'email', 'password'];
-    let [isError, setError] = useState(false);
-
-    let onSubmit = (values) => submitForm(values, template, 'POST', '/', endRegistration, setError);
-    return (<Fragment>
-        {isError &&
-        <div className='container'><p className='text text_error'>Something was wrong.</p></div>}
-        <Form className={className}
-              input={[{text: 'login'}, {email: 'email'}, {password: 'password'}, {password: 'repeat the password'}]}
-              initial={{login: '', email: '', password: '', repeatThePassword: ''}}
-              validationSchema={validationSchema_newUser}
-              onSubmit={onSubmit}/>
-    </Fragment>)
-};
+import {Login} from "../helpers/components/loginForm";
+import {NewUserForm} from "../helpers/components/newUserForm";
 
 export const LoginPage = () => {
 
-    const [displayLoginForm, setDisplayLoginForm] = useState(true);
+    const [displayLoginForm, setDisplayLoginForm] = useState(false);
     let [popup, showPopup] = useState(false);
 
     let loginFormClassName, newUserFormClassName, text;
@@ -68,19 +31,19 @@ export const LoginPage = () => {
     return (
         <Card
             headerText='Hi, stranger!'
-            render={() => <Fragment>
-                {popup
-                    ? <Popup text='sadf' onclick={() => showPopup(false)} textForButton='Login'/>
-                    : <Fragment>
-                        <Login className={loginFormClassName}/>
-                        <NewUserForm className={newUserFormClassName} endRegistration={endRegistration}/>
-                        <div className='container'>
-                            <p className='text text_link'
-                               onClick={() => setDisplayLoginForm(!displayLoginForm)}>{text}</p>
-                        </div>
-                    </Fragment>
-                }
-            </Fragment>}
+            render={() =>
+                <Fragment>
+                    <Login className={loginFormClassName}/>
+                    <NewUserForm className={newUserFormClassName} endRegistration={endRegistration}/>
+                    <div className='container'>
+                        <p className='text text_link'
+                           onClick={() => setDisplayLoginForm(!displayLoginForm)}>{text}</p>
+                    </div>
+                    {popup
+                    ? <Popup className='popup popup_success' text='Success! Now you can log in.' />
+                    :null}
+                </Fragment>
+            }
         />
     )
 };
