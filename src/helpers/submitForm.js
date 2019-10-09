4838154f3cca42bfcfabf073d42ba0647203e4b4
hempy <hempy.scrymetto@gmail.com>
2019-10-09 20:14:59 +0300

    submitForm() with async/await

--- src/helpers/submitForm.js
+++ src/helpers/submitForm.js
@@ -1,19 +1,18 @@
-import {prepareDataForRequest} from "./prepareDataForRequest";
 import axios from "axios";
 
-export const submitForm = (values, template, url, onSuccess, onError) => {
-    let data = prepareDataForRequest(template, values);
-     axios.get(url)
-        .then(result => {
-            if (result.status >= 200 && result.status < 300 ) {
-                onSuccess(result.data)
-            } else {
-                console.log(result)
-                onError(result.status)
-            }
-        })
-        .catch(e => {
-            onError(e);
-            throw new Error(e)
-        });
+export const submitForm = async (data, url, onSuccess, onError) => {
+    try {
+        const response = await axios.get(url);
+        console.log(response.status)
+        if (response.status >= 200 && response.status < 300) {
+            console.log('onSuccess')
+            onSuccess(response.data)
+        } else {
+            console.log('onError')
+            onError(response.status)
+        }
+    } catch (e) {
+        onError(e);
+        throw new Error(e)
+    }
 };
