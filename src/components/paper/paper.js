1dded54990b399fbe7fc04747f9df0e53b3ab6a9
hempy <hempy.scrymetto@gmail.com>
2019-10-21 22:14:55 +0300

    new component <Paper/>

---
+++ src/components/paper/paper.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './paper.css'
+import '../text/text_additional.css'
+import '../text/text_header.css'
+
+import {Players} from '../players/players'
+
+export const Paper = (props) => {
+    const {admin, name, players} = props;
+    return (
+        <div className='paper'>
+            <p className='text text_additional'>Admin of this room: {admin}</p>
+            <h2 className='text text_header'>{name}</h2>
+            <Players players={players}/>
+        </div>
+    )
+};
+
+Paper.propTypes = {
+    admin: PropTypes.string,
+    name: PropTypes.string,
+    players: PropTypes.array
+};
