848f5d325caf873ad44cde68b52583fe6d622c01
hempy <hempy.scrymetto@gmail.com>
2019-10-21 22:13:47 +0300

    new logic in rooms()

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../App.css';
 import {Card} from "../components/card/card";
 import {Button} from "../components/button/button";
 import {useGlobal} from "../store"
+import {getRooms} from "../helpers/requests/getRooms";
+import {useAuth} from "../helpers/auth&route/authContext";
+import {Paper} from "../components/paper/paper";
 
 export function Rooms() {
 
+    useEffect(() => {
+        getRooms(user, onSuccess, onError)
+            .then(() => setUploaded(true));
+    }, []);
+
+    let [isUploaded, setUploaded] = useState(false);
     let [globalState, globalActions] = useGlobal();
-    // let rooms = globalState.state.rooms;
-    console.log(globalState)
-    let addNew = (room) => {
-        globalActions.addNewRoom(room)
-    }
-    let room = {name: 'name'};
+    let rooms = globalState.rooms;
+    const {user} = useAuth();
+    let onError = (e) => console.log(e)
+    let onSuccess = (rooms) => {
+        globalActions.addRoomsFromServer(rooms);
+    };
+    // let addNew = (room) => {
+    //     globalActions.addNewRoom(room)
+    // }
+    // let room = {name: 'name'};
     return (
-        <div className="App">
             <Card headerText='Your rooms'
                   render={() => (
                       <div>
-                          <Button text='Hey' className='button' onClick={() => addNew(room)}/>
+                          {isUploaded ?
+                              rooms.map(room => {
+                                  console.log(rooms)
+                                  console.log(room)
+                                  return (
+                                      <Paper
+                                          key={room.id}
+                                          players={room.users}
+                                          name={room.name}
+                                          admin={room.creatorId}/>
+                                  )
+                              })
+                              : null}
+                          <Button text='Hey' className='button' /*onClick={() => addNew(room)}*//>
                       </div>
                   )}
             />
-        </div>
     )
 }
