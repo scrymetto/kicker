ff4e7c0af36d361370c01c7e223535e899f09566
hempy <hempy.scrymetto@gmail.com>
2019-10-24 18:53:26 +0300

    test 'render without problem' for <App/> works

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import App from "../src/App";
-import {act} from 'react-dom/test-utils';
+// import {act} from 'react-dom/test-utils';
 
 //TODO What the f*** ?! how test it?
 
 describe('App', () => {
     let wrapper;
-    // it('should render without problem', () => {
-    //     act(() => {
-    //         wrapper = shallow(<App/>);
-    //     });
-    //     expect(wrapper.find(App).length).to.equal(1)
-    // });
+    it('should render without problem', () => {
+        wrapper = shallow(<App/>);
+        let app = wrapper.findWhere(node => node.hasClass('App') === true);
+        expect(app.length).to.equal(1)
+    });
     it('should contain <Header\/> with class header_main', () => {
-        act(() => {
-            wrapper = shallow(<App/>);
-        });
+        wrapper = shallow(<App/>);
         let header = wrapper.findWhere(node => node.props().text === 'Let\'s play kicker!');
         expect(header.length).to.equal(1)
     });
     // it('should have empty context, when it starts', () => {
-    //     act(() => {
     //         wrapper = mount(<App/>);
-    //     });
     //     console.dir(wrapper)
     //     let header = wrapper.findWhere(node => node.text() === 'Hi, stranger!');
     //     expect((header).length).to.equal(1);
