29f54693b1e0f7eb2e9ef3c3d07adc4b24263269
hempy <hempy.scrymetto@gmail.com>
2019-10-24 18:53:52 +0300

    tests for <Players/>

---
+++ tests/players.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Players} from '../src/components/players/players'
+
+describe('Players ', () => {
+    let label = <p className='text text_additional'>Players: </p>;
+    let emptyLabel = <p className='text text_additional'>There are no players yet.</p>;
+    let users = ['Hulk', 'Iron Man', 'Ms. Marvel'];
+    let noUsers = [];
+    it('should have text \'Players\'', () => {
+        let wrapper = shallow(<Players players={users}/>);
+        expect(wrapper.contains(label)).to.equal(true)
+    });
+    it('should have other text if props. array is empty', () => {
+        let wrapper = shallow(<Players players={noUsers}/>);
+        expect(wrapper.contains(emptyLabel)).to.equal(true)
+    });
+    it('should render all users', () => {
+        let wrapper = shallow(<Players players={users}/>);
+        expect(wrapper.children().length).to.equal(users.length + 1)
+    });
+});
