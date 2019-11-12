2e56733ee1df7f6408eb499fc1da2af8704d8d1a
hempy <hempy.scrymetto@gmail.com>
2019-11-12 19:10:46 +0300

    start to test prepareGamesForTable()

--- src/helpers/prepareGamesForTable.js
+++ src/helpers/prepareGamesForTable.js
@@ -12,7 +12,7 @@ export const prepareGamesForTable = (games) => {
         let secondTeamPlayers = prepareStringWithPlayers(secondTeam.players);
         let firstTeamCell =
             <Fragment>
-                <p className='text text_oblique'>
+                <p className='text text_oblique' test='team'>
                     {firstTeamName}
                 </p>
                 <p className='text text_additional'>
@@ -21,14 +21,14 @@ export const prepareGamesForTable = (games) => {
             </Fragment>;
         let secondTeamCell =
             <Fragment>
-                <p className='text text_oblique'>
+                <p className='text text_oblique' test='opponent'>
                     {secondTeamName}
                 </p>
                 <p className='text text_additional'>
                     {secondTeamPlayers}
                 </p>
             </Fragment>;
-        let score = firstTeamScore + ':' + secondTeamScore;
+        let score = <p className='text' test='score'>{firstTeamScore} + ':' + {secondTeamScore}</p>;
         return {
             team: firstTeamCell,
             score: score,
@@ -37,103 +37,3 @@ export const prepareGamesForTable = (games) => {
     })
 };
 
-// function makeFirstLetterUppercase(string) {
-//     return string[0].toUpperCase() + string.slice(1)
-// }
-//
-// const prepareStringWithPlayers = (array) => {
-//     let string = '';
-//     array.forEach((player, index) => {
-//         let playerFunction = player.function;
-//         if (playerFunction) {
-//             let article = player.function === 'attacker' ? ' an ' : ' a ';
-//             string += makeFirstLetterUppercase(player.name) + ' as' + article + player.function;
-//         } else string += makeFirstLetterUppercase(player.name);
-//         if (index === array.length - 2) {
-//             string += ' and '
-//         } else if (index !== array.length - 1) {
-//             string += ', '
-//         }
-//     });
-//     return string
-// };
-
-// let columns = ['team', 'score', 'opponent'];
-// const games = [
-//     {
-//         "id": "0",
-//         "firstTeam": {
-//             "id": "0",
-//             "name": 'dfgh',
-//             "players": [
-//                 {
-//                     "id": "1",
-//                     "name": "Vasja",
-//                     "function": "attacker"
-//                 },
-//                 {
-//                     "id": "2",
-//                     "name": "Kolja",
-//                     "function": "defender"
-//                 },
-//                 {
-//                     "id": "1",
-//                     "name": "Vasja",
-//                     "function": ""
-//                 },
-//             ],
-//             "points": 8
-//         },
-//         "secondTeam": {
-//             "id": "1",
-//             "name": 'sdzfg',
-//             "players": [
-//                 {
-//                     "id": "3",
-//                     "name": "Petja",
-//                     "function": "solo"
-//                 },
-//             ],
-//             "points": 10
-//         }
-//     },
-//     {
-//         "id": "0",
-//         "firstTeam": {
-//             "id": "0",
-//             "name": 'dfgh',
-//             "players": [
-//                 {
-//                     "id": "1",
-//                     "name": "Vasja",
-//                     "function": "attacker"
-//                 },
-//                 {
-//                     "id": "2",
-//                     "name": "Kolja",
-//                     "function": "defender"
-//                 },
-//                 {
-//                     "id": "1",
-//                     "name": "Vasja",
-//                     "function": ""
-//                 },
-//             ],
-//             "points": 8
-//         },
-//         "secondTeam": {
-//             "id": "1",
-//             "name": 'sdzfg',
-//             "players": [
-//                 {
-//                     "id": "3",
-//                     "name": "Petja",
-//                     "function": "solo"
-//                 },
-//             ],
-//             "points": 10
-//         }
-//     }
-// ];
-//
-// prepareGamesForTable(games, columns);
--- tests/prepareGamesForTable.test.js
+++ tests/prepareGamesForTable.test.js
@@ -1,2 +1,29 @@
-let data = require('../__mocks__/games');
+import {prepareGamesForTable} from "../src/helpers/prepareGamesForTable";
+import React, {Fragment} from "react";
 
+let hundredYearsWar = require('../__mocks__/games.json');
+
+describe('prepareGamesForTable() ', () => {
+    let component;
+    beforeEach(()=>{
+        let games = prepareGamesForTable(hundredYearsWar);
+        component = mount(
+            <div>
+                {games.map((game, index)=>
+                    <Fragment key={index}>{game.team} {game.score} {game.opponent}</Fragment>)}
+            </div>)
+    });
+    afterEach(()=>{
+        component.unmount();
+    });
+    it('should contain not-empty fields \'team\', \'score\', \'opponent\'', () => {
+        let team = component.find('p').filterWhere(node => node.props().test === 'team');
+        let opponent = component.find('p').filterWhere(node => node.props().test === 'opponent');
+        let score = component.find('p').filterWhere(node => node.props().test === 'score');
+
+        expect(team.length).to.equal(hundredYearsWar.length);
+        expect(opponent.length).to.equal(hundredYearsWar.length);
+        expect(score.length).to.equal(hundredYearsWar.length);
+    });
+
+});
