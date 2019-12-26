20b5c1e9c183229edbf3af267017e2b5f83281df
hempy <hempy.scrymetto@gmail.com>
2019-12-26 17:53:58 +0300

    Page404 works!

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,26 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {Redirect} from "react-router-dom";
 
 import {Card} from "../components/card/card";
 import {Button} from "../components/button/button";
 
+import '../components/container/grid_1-1.css'
+
 export const Page404 = () => {
+    const [onclick, setOnclick] = useState(false);
     return <Card headerText='Oooooops...'
                  render={() =>
                      <Fragment>
-                         <div>
-                             <p></p>
-                             <p className='text'>This page doesn't exist.</p>
+                         {onclick && <Redirect to="/rooms"/>}
+                         <div className='container grid_1-1'>
+                             <p className='text' style={{fontSize: '5em', margin:"auto"}}>&#129335;</p>
+                             <p className='text' style={{margin:"auto"}}>This page doesn't exist.</p>
                          </div>
-                         <Button className='button' text='Go to home page' onClick={()=><Redirect to="/rooms"/>}/>
+                         <Button className='button'
+                                 text='Go to home page'
+                                 onClick={() => setOnclick(true)}
+                                 style={{margin: '10px auto'}}
+                         />
                      </Fragment>
                  }/>
 };
