7d6d1cf032eb7db07ccb6a8ce0a199b793373294
hempy <hempy.scrymetto@gmail.com>
2020-02-18 16:48:21 +0300

    refactor of the getTheLastGameId-request

--- src/helpers/requests/getTheLastGameId.js
+++ src/helpers/requests/getTheLastGameId.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const getTheLastGameId = async (user, roomId) => {
 
-        const answer = await axios.get(`/api/game/last-id?roomId=${roomId}`, {auth: user.auth});
-        return answer.data.value
+    const answer = await axios.get(`/api/game/last-id?roomId=${roomId}`, {auth: user.auth});
+    return answer.data.value
 };
